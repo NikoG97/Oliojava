4d17c579d73aba7d90a4789ca7f08537ea278a00
NikoG97 <nikogron@hotmail.com>
2021-01-20 14:49:06 +0200

    init

---
+++ src/Doge.java
@@ -0,0 +1,36 @@
+package src;
+
+public class Doge(String n){
+
+    private String name = "Doggo";
+
+
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String n){
+        name = n;
+    }
+
+
+}
+
+
+package src;
+
+public class Doge(String n){
+
+    private String name = "Doggo";
+
+
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String n){
+        name = n;
+    }
+
+
+}

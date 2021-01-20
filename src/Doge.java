1b5ad9a1e528c7e5846d330e6fe129bb10bae168
NikoG97 <nikogron@hotmail.com>
2021-01-20 14:49:23 +0200

    push

--- src/Doge.java
+++ src/Doge.java
@@ -15,22 +15,3 @@ public class Doge(String n){
 
 
 }
-
-
-package src;
-
-public class Doge(String n){
-
-    private String name = "Doggo";
-
-
-    public String getName(){
-        return name;
-    }
-    
-    public void setName(String n){
-        name = n;
-    }
-
-
-}
